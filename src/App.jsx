03a909616da2e29bf5feeb348c5fb44272e8ef13
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-02-04 15:14:29 -0300

    adoptme WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
